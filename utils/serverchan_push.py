99dfc9d45b67d3ceef9a46a748e400433c68ffa6
Rocky <maorock@163.com>
2022-07-10 10:28:39 +0800

    Update serverchan_push.py

--- utils/serverchan_push.py
+++ utils/serverchan_push.py
@@ -10,10 +10,11 @@ def push_to_wechat(text,desp,secretKey):
     :param text: 标题
     :param desp: 内容
     :return resp: json
-    """
-    url = f'http://sc.ftqq.com/{secretKey}.send'
+    """    
+    url = f'http://sc.ftqq.com/{secretKey}.send?title=zdm.CheckIn&desp=messagecontent'
     session = requests.Session()
-    data = {'title':text,'desp':'messagecontent'}
+    #data = {'text':text,'desp':desp}
+    data = {}
     resp = session.post(url,data = data)
     print(text)
     print(resp)
